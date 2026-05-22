4d4dc9cf65965da80607320a67d7ac3aa9856093
seonjiwon <tommys915@gmail.com>
2026-05-22 16:49:55 +0900

    feat: Session 받는 부분 수정 + 이상 로직 제거

--- hangang-pay-be/src/main/java/family/fisa/hangangpay/domain/transaction/dto/response/ExchangePreviewResponse.java
+++
@@ -1,9 +0,0 @@
-package family.fisa.hangangpay.domain.transaction.dto.response;
-
-import java.math.BigDecimal;
-
-public record ExchangePreviewResponse(
-        BigDecimal exchangeAmount,
-        BigDecimal discountRate,
-        BigDecimal discountAmount,
-        BigDecimal actualReceiveAmount) {}
